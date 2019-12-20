f7307beb5ccfa2fd88a11cc98a989c43acf0cb63
sesepulveda17 <sesepulveda@ug.uchile.cl>
2019-12-20 11:52:45 -0300

    changes in urls

--- django_practice/mysite/polls/views.py
+++ django_practice/mysite/polls/views.py
@@ -1,13 +1,22 @@
 from django.http import HttpResponse
 #from django.template import loader
 #atajo para cargar contexto al template con render
-from django.shortcuts import render
+#from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
-
+'''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+'''
 def detail(request,question_id):
-    return HttpResponse("You're looking any question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
