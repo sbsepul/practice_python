97b41fce1f41ca774938a167fe91b1374d33550b
sesepulveda17 <sesepulveda@ug.uchile.cl>
2019-12-20 11:30:08 -0300

    shorcout added

--- django_practice/mysite/polls/views.py
+++ django_practice/mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+#atajo para cargar contexto al template con render
+from django.shortcuts import render
 
 from .models import Question
 
@@ -17,8 +19,5 @@ def vote(request,question_id):
 #Query in the API for the votes
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context,request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
