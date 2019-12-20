2d13f77da49695062205ff57ac749a015be7a536
sesepulveda17 <sesepulveda@ug.uchile.cl>
2019-12-20 11:13:56 -0300

    3rd attempt to add gitignore

--- django_practice/mysite/polls/views.py
+++ django_practice/mysite/polls/views.py
@@ -13,3 +13,9 @@ def results(request, question_id):
 
 def vote(request,question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+#Query in the API for the votes
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
