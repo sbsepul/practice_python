68c50e4024da6d6bce7eabda4103f1185e946253
sesepulveda17 <sesepulveda@ug.uchile.cl>
2019-12-21 12:35:50 -0300

    Part 6 Tutorial finished

--- django_practice/mysite/polls/views.py
+++ django_practice/mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
